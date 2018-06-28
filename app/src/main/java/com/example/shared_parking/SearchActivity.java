b39a4f22eba1663891d70100a383d9dd04cc8acb
Simon Englert <simon.englert@stud-mail.uni-wuerzburg.de>
2018-06-28 16:55:24 +0200

    first commit

---
+++ app/src/main/java/com/example/shared_parking/SearchActivity.java
@@ -0,0 +1,11 @@
+package com.example.shared_parking;
+
+import android.os.Bundle;
+
+public class SearchActivity extends MainActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_search);
+    }
+}
---
+++ app/src/main/java/com/example/shared_parking/SpotsActivity.java
@@ -0,0 +1,11 @@
+package com.example.shared_parking;
+
+import android.os.Bundle;
+
+public class SpotsActivity extends MainActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spots);
+    }
+}
